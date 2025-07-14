445523d6d9a7b63764dc790d464cd1bcb8138445
hammadsyeda <146228660+hammadsyeda@users.noreply.github.com>
2025-07-14 23:56:37 +0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,19 @@
+number1 = int(input("enter the value of no. ="))
+number2 = int (input("enter the value of second no. ="))
+
+add = number1 + number2
+print ("the sum of number" ,number1  ,"and" ,number2 ,"is" ,add)
+
+
+sub = number1 - number2
+print ("the substraction of number" ,number1  ,"and" ,number2 ,"is" ,sub)
+
+
+div = number1 / number2
+print ("the divide of number" ,number1  ,"and" ,number2 ,"is" ,div)
+
+
+multiply = number1 ** number2
+print ("the multiplication of number" ,number1  ,"and" ,number2 ,"is" ,multiply)
+
+print(":-)")
